90f49262d66481b26367de3aea79d19429b8c394
melis_m <matteo.melis@epitech.eu>
2018-12-21 15:21:10 +0100

    Added some imports to nbuild package

--- nbuild/__init__.py
+++ nbuild/__init__.py
@@ -1,4 +1,7 @@
-#!/usr/bin/env python3.6
-# -*- coding: utf-8 -*-
-
-pass
+from nbuild.stdenv.autotools.make import do_make
+from nbuild.stdenv.autotools.autoconf import do_configure
+from nbuild.stdenv.autotools import build_autotools_package
+from nbuild.stdenv.install import exclude_dirs, keep_only
+from nbuild.stdenv.package import package
+from nbuild.stdenv.build import current_build
+from nbuild.stdenv.fetch import fetch_urls

c4a6041d582df1d0b2bd0057bc6673dbf9117c12
melis_m <matteo.melis@epitech.eu>
2019-01-29 18:05:56 +0100

    Added checks on exec permissions on binaries and libs

--- nbuild/checks/check.py
+++ nbuild/checks/check.py
@@ -1,2 +1,10 @@
+from nbuild.checks.executable import check_exec
+from nbuild.log import ilog
+
+
 def check_package(pkg):
-    print(pkg)
+    suffix = pkg.name.split('-')[-1] if '-' in pkg.name else None
+    ilog(f"Checking package installed at {pkg.install_dir}", indent=False)
+    if suffix is None or suffix == 'bin':
+        check_exec(pkg)
+    ilog("All checks done")

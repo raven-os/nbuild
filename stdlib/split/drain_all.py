f10e910e6f74fd1354450550fddd4faf94b22c39
Benjamin Grange <benjamin.grange@epitech.eu>
2019-05-27 15:16:08 +0100

    Fix english typo in documentation

--- stdlib/split/drain_all.py
+++ stdlib/split/drain_all.py
@@ -7,9 +7,10 @@ A very basic and primitive splitter that drains all the output files of a build 
 
 import stdlib.build
 import stdlib.package
+from typing import Dict
 
 
-def drain_all():
+def drain_all() -> Dict[str, stdlib.package.Package]:
     """
     A very basic and primitive splitter that drains all the output files of a build into a single package, named after the
     :py:class:`.BuildManifest`.
@@ -17,7 +18,6 @@ def drain_all():
 
     :returns: A dictionary, with the generated package's :py:func:`~stdlib.package.PackageID.short_name` as the key, and the
         associated :py:class:`.Package` as the value.
-    :returntype: ``Dict`` [ ``str`` , :py:class:`.Package` ]
     """
 
     build = stdlib.build.current_build()

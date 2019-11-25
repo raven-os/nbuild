7a33eff6791dbfddd59e1a38accd2519b9f2a754
Clément "Doom" Doumergue <clement.doumergue@epitech.eu>
2019-11-25 14:47:52 +0100

    Add the drain_*_into splitters (#62)

--- stdlib/split/drain_all.py
+++ stdlib/split/drain_all.py
@@ -7,11 +7,27 @@ A very basic and primitive splitter that drains all the output files of a build 
 
 import os
 import stdlib.build
-import stdlib.package
+from stdlib.package import Package, PackageID
 from typing import Dict
 
 
-def drain_all() -> Dict[str, stdlib.package.Package]:
+def drain_all_into(target_package: Package) -> Dict[str, Package]:
+    """
+    A very basic and primitive splitter that drains all the output files of a build into a given package.
+
+    :param target_package: the :py:class:`.Package` to drain into
+
+    :returns: A dictionary, with the given package's :py:func:`~stdlib.package.PackageID.short_name` as the key, and the
+        associated :py:class:`.Package` as the value.
+    """
+    target_package.drain('*')
+
+    return {
+        target_package.id.short_name(): target_package
+    }
+
+
+def drain_all() -> Dict[str, Package]:
     """
     A very basic and primitive splitter that drains all the output files of a build into a single package, named after the
     :py:class:`.BuildManifest`.
@@ -23,18 +39,46 @@ def drain_all() -> Dict[str, stdlib.package.Package]:
 
     build = stdlib.build.current_build()
 
-    package = stdlib.package.Package(
-        stdlib.package.PackageID(build.manifest.metadata.name),
+    package = Package(PackageID(build.manifest.metadata.name))
+
+    return drain_all_into(package)
+
+
+def drain_all_with_doc_into(regular_package: Package, doc_package: Package) -> Dict[str, Package]:
+    """
+    A very basic and primitive splitter that drains all the output files of a build but the documentation into a given package,
+    and all the documentation in a second given package.
+
+    :param regular_package: the :py:class:`.Package` to drain into
+    :param doc_package: the :py:class:`.Package` to drain the documentation into
+
+    :returns: A dictionary, with the given packages' :py:func:`~stdlib.package.PackageID.short_name` as keys, and the
+        associated :py:class:`.Package` as values.
+    """
+    target = os.environ['TARGET']
+
+    regular_package.move('{,usr/local/}{,s}bin/*', 'usr/bin/')
+    regular_package.move('usr/sbin/*', 'usr/bin/')
+    regular_package.move(f'usr/{target}/sbin/*', f'usr/{target}/bin/')
+
+    regular_package.move('{lib,usr/local/lib}{,64}/*', 'usr/lib64/')
+    regular_package.move('{lib,usr/local/lib}32/*', 'usr/lib32/')
+    regular_package.move(f'usr/{target}/lib/*', f'usr/{target}/lib64/')
+
+    doc_package.drain(
+        'usr/share/doc/',
+        'usr/share/info/',
     )
 
-    package.drain('*')
+    regular_package.drain('*')
 
     return {
-        package.id.short_name(): package
+        regular_package.id.short_name(): regular_package,
+        doc_package.id.short_name(): doc_package,
     }
 
 
-def drain_all_with_doc() -> Dict[str, stdlib.package.Package]:
+def drain_all_with_doc() -> Dict[str, Package]:
     """
     A very basic and primitive splitter that drains all the output files of a build but the documentation into a single package,
     named after the :py:class:`.BuildManifest`, and all the documentation in a second package, named like the first one, except with
@@ -46,35 +90,12 @@ def drain_all_with_doc() -> Dict[str, stdlib.package.Package]:
     """
 
     build = stdlib.build.current_build()
-    target = os.environ['TARGET']
 
-    package = stdlib.package.Package(
-        stdlib.package.PackageID(build.manifest.metadata.name),
-    )
+    package = Package(PackageID(build.manifest.metadata.name))
 
-    package.move('{,usr/local/}{,s}bin/*', 'usr/bin/')
-    package.move('usr/sbin/*', 'usr/bin/')
-    package.move(f'usr/{target}/sbin/*', f'usr/{target}/bin/')
-
-    package.move('{lib,usr/local/lib}{,64}/*', 'usr/lib64/')
-    package.move('{lib,usr/local/lib}32/*', 'usr/lib32/')
-    package.move(f'usr/{target}/lib/*', f'usr/{target}/lib64/')
-
-    documentation = stdlib.package.Package(
-        stdlib.package.PackageID(
-            f'{build.manifest.metadata.name}-doc',
-        ),
+    documentation = Package(
+        PackageID(f'{build.manifest.metadata.name}-doc'),
         description=f"Offline documentation of {package.id.short_name()}."
     )
 
-    documentation.drain(
-        'usr/share/doc/',
-        'usr/share/info/',
-    )
-
-    package.drain('*')
-
-    return {
-        package.id.short_name(): package,
-        documentation.id.short_name(): documentation,
-    }
+    return drain_all_with_doc_into(package, documentation)

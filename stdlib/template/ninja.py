4dbac6401be9a5d77d9ef6d0fa6c1f837c99fdd2
Arignir <Arignir@users.noreply.github.com>
2019-11-25 16:36:37 +0100

    Add an exhaustiv template for meson+ninja. (#61)

--- stdlib/template/ninja.py
+++ stdlib/template/ninja.py
@@ -2,6 +2,7 @@
 # -*- coding: utf-8 -*-
 """Provides a small, partial template that wraps the ``ninja`` subcommands."""
 
+import os
 import stdlib
 
 
@@ -16,11 +17,45 @@ def ninja(
     :param args: Any extra arguments to give to ``ninja``.
     :param binary: The command or path to use. The default value is ``ninja``.
     :param folder: The target folder. The default value is ``.``.
-    :param backend: Backend to use. Default: ``ninja``.
-    :param build_type: Build type to use. Default: ``release``.
-    :param build_folder: The build folder when object files should be put. Default: ``.``.
-    :param source_folder: The source folder to setup. Default: ``.``.
     :param fail_ok: If ``False``, the execution is aborted if ``ninja`` fails.
         The default value is ``False``.
     """
     stdlib.cmd(f'''{binary} -C "{folder}" {' '.join(args)} ''', fail_ok=fail_ok)
+
+
+def ninja_test(
+    *args: str,
+    binary: str = 'ninja',
+    folder: str = '.',
+    fail_ok: bool = False,
+):
+    """Run ``ninja test``.
+
+    :param args: Any extra arguments to give to ``ninja``.
+    :param binary: The command or path to use. The default value is ``ninja``.
+    :param folder: The target folder. The default value is ``.``.
+    :param fail_ok: If ``False``, the execution is aborted if ``ninja`` fails.
+        The default value is ``False``.
+    """
+    ninja('test', *args)
+
+
+def ninja_install(
+    *args: str,
+    target: str = '',
+    binary: str = 'ninja',
+    folder: str = '.',
+    fail_ok: bool = False,
+):
+    """Run ``ninja install``.
+
+    :param args: Any extra arguments to give to ``ninja``.
+    :param binary: The command or path to use. The default value is ``ninja``.
+    :param folder: The target folder. The default value is ``.``.
+    :param fail_ok: If ``False``, the execution is aborted if ``ninja`` fails.
+        The default value is ``False``.
+    """
+    with stdlib.pushenv():
+        if not os.environ['DESTDIR']:
+            os.environ['DESTDIR'] = stdlib.build.current_build().install_cache
+    ninja('install', *args)
